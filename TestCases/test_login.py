527e88ab7219dc96583a5487467444f3ddcecd67
santoshdg39@gmail.com <your_email@example.com>
2025-12-27 23:07:34 +0530

    Verify employee data import successfully using an CSV file #4 and some code refractor

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -1,5 +1,7 @@
 import allure
 import pytest
+
+import TestData.messages
 from TestCases.basetest import BaseTest
 
 
@@ -7,16 +9,18 @@ class TestLogin(BaseTest):
 
     # Verify that admin can log in using valid credentials.
     @pytest.mark.sanity
-    def test_001_login(self, load_pages):
-        self.log.info("created login page")
+    def test_001_login_with_valid_password(self, load_pages):
+        self.log.info("TEST STARTED: login_with_valid_password")
         self.loginPage.login(self.username, self.password)
         assert self.dashboardPage.is_dashboard_displayed(), "Login failed: Dashboard not displayed"
 
     # Verify that login fails with invalid credentials and an appropriate error message is shown
     @pytest.mark.sanity
-    def test_002_login(self, load_pages):
-        self.log.info("created login page")
+    def test_002_login_with_invalid_password(self, load_pages):
+        self.log.info("TEST STARTED: login_with_invalid_password")
         self.loginPage.login(self.username, "admin123dd")
-        assert self.loginPage.is_login_error_displayed(), "Invalid credentials message is not displayed"
+        login_error_message = self.loginPage.get_text_value(self.loginpagelocators.ERROR_MESSAGE)
+        assert TestData.messages.invalid_password_error_message_text in login_error_message, "Invalid credentials " \
+                                                                                             "message is not displayed "
 
 
